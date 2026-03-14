7650c53fc38cc197de024346540159b4ab20c426
“Nelson” <nelson@gmail.com>
2026-03-14 21:09:23 +0100

    3 klassen aangemaakt om data te ontvangen en te controleren en de routes aangemaakt voor de post en get routes.

---
+++ Webapp/routes/Api.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\WeatherController;
+use Illuminate\Support\Facades\Route;
+
+
+// route voor weerdata ontvangen wat een POST is.
+Route::post('/weather', [WeatherController::class, 'store']);
+
+
+// route voor opvragen (get)
+Route::get('/weather', [WeatherController::class, 'index']);
+
+// Route voor het opvragen van data van een specifiek station
+Route::get('/weather/station/{stationId}', [WeatherController::class, 'getByStation']);
+
+
+
