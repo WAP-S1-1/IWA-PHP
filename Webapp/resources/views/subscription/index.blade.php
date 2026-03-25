8154a4ad79d90810a4cc1b625e3d25143cd8f5a6
Anh-Thy04 <anhthytran2309@gmail.com>
2026-03-25 22:36:33 +0100

    Update index.blade.php

--- Webapp/resources/views/subscription/index.blade.php
+++ Webapp/resources/views/subscription/index.blade.php
@@ -11,12 +11,35 @@
             display: inline-block;
             vertical-align: top;
         }
+
     </style>
 
     <div class="card shadow-lg m-2 rounded-5"
          style="width:82%;height:700px; background-color:rgba(255,255,255,0.7)">
-        <div class="d-flex align-items-start">
-            <h2 class="my-3 e-4 align-self-baseline">Abonnementen</h2>
+        <div class="row p-3">
+            <div class="col-sm-2 m-5 d-flex flex-wrap gap-3">
+                <p class="mb-0">ID</p>
+                <p class="mb-0">Bedrijf</p>
+                <hr class="w-100 my-0">
+            </div>
+            <div class="col-sm-2 m-5 d-flex flex-wrap gap-3">
+                <p class="mb-0">Startdatum</p>
+                <p class="mb-0">Einddatum</p>
+                <hr class="w-100 my-0">
+            </div>
+            <div class="col-sm-5 m-5 d-flex flex-wrap gap-1" >
+                <p class="mb-0">Abonnement</p>
+                <p class="mb-0">Omschrijving </p>
+                <p class="mb-0">Prijs</p>
+                <p class="mb-0">Notities</p>
+                <hr class="w-100 my-0">
+            </div>
+        </div>
+        <div class="w-100">
+            <div class="row p-5 g-4 justify-content-center">
+
+            </div>
+        </div>
     </div>
 @endsection
 
