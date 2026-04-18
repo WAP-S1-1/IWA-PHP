56e9b0a3aa80c9f8a54a492a49d595d5b8fd5bbe
Stef <stbuwalda@gmail.com>
2026-04-18 20:02:36 +0200

    Small fix

--- Webapp/app/Http/Controllers/MonitoringController.php
+++ Webapp/app/Http/Controllers/MonitoringController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Measurement;
 use App\Models\Station;
-use App\Models\Measurement;
 use Illuminate\Http\Request;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Carbon;
