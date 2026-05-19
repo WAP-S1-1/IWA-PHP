dd1abb916a5a3a9b4f7c059da3e31d0e4eca1dc9
Stef <stbuwalda@gmail.com>
2026-05-19 11:17:51 +0200

    Fixed Vue and Vite

--- zalora/vite.config.js
+++ zalora/vite.config.js
@@ -16,6 +16,16 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.esm-bundler.js',
+        },
+    },
+    define: {
+        __VUE_OPTIONS_API__: true,
+        __VUE_PROD_DEVTOOLS__: false,
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+    },
     server: {
         watch: {
             ignored: ['**/storage/framework/views/**'],
