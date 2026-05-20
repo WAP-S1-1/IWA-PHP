2a46e0e9ec1d8818cbd979a45b65758b3ceca8a6
DaanoGames <d.stikfort@icloud.com>
2026-05-20 22:30:51 +0200

    added Vue files
    the Vue files are just a start, i can't test them yet, but i made them so we can build further out from them

---
+++ zalora/resources/views/components/Navbar.vue
@@ -0,0 +1,74 @@
+<template>
+    <header class="header">
+        <h1>Zalora</h1>
+        <button class="menu-btn"
+        @click="menuOpen = !menuOpen">
+            ☰
+        </button>
+        <nav class="nav"
+        :class="{ show: menuOpen}">
+            <a href="#">Home</a>
+            <a href="#">Login</a>
+        </nav>
+    </header>
+</template>
+<script setup>
+import { ref } from 'vue'
+
+const menuOpen = ref(false)
+</script>
+<style scoped>
+.header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    padding: 1rem;
+    background: white;
+}
+
+.menu-btn {
+    font-size: 1.5rem;
+    border: none;
+    background: none;
+}
+
+.nav {
+    display: none;
+
+    position: absolute;
+    top: 60px;
+    left: 0;
+
+    width: 100%;
+    background: white;
+
+    flex-direction: column;
+}
+
+.nav.show {
+    display: flex;
+}
+
+.nav a {
+    padding: 1rem;
+    text-decoration: none;
+    color: black;
+}
+
+/* DESKTOP */
+@media (min-width: 1024px) {
+
+    .menu-btn {
+        display: none;
+    }
+
+    .nav {
+        display: flex;
+        position: static;
+        flex-direction: row;
+        width: auto;
+        gap: 1rem;
+    }
+}
+</style>
