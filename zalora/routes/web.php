8f748bf5488f9dc4650cfe80cbaab77b3e611e5a
DaanoGames <d.stikfort@icloud.com>
2026-05-26 11:35:02 +0200

    added map and routing for map

--- zalora/routes/web.php
+++ zalora/routes/web.php
@@ -6,10 +6,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/index', function () {
-    return view('index');
-});
-
 Route::get('/home', function () {
     return view('home');
+});
+
+Route::get('/map', function () {
+    return view('map');
 });
