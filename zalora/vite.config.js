c2df3f3623673b061d16fc20a31e1677b9d42b6d
Stef <stbuwalda@gmail.com>
2026-06-01 19:09:17 +0200

    Fixed webpage error

--- zalora/vite.config.js
+++ zalora/vite.config.js
@@ -12,13 +12,7 @@ export default defineConfig({
         }),
         vue(),
         tailwindcss(),
-        vue(),
     ],
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.esm-bundler.js',
-        },
-    },
     define: {
         __VUE_OPTIONS_API__: true,
         __VUE_PROD_DEVTOOLS__: false,
